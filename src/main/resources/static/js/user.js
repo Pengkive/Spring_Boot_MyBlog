0e4dabe0739f290dbaa1088353c1d825acd9b36e
fennec <fennecrami@gmail.com>
2022-01-17 00:54:02 +0900

    중복회원 회원가입시 alert 추가

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -28,9 +28,14 @@
 					contentType: "application/json; charset=utf-8",// body데이터가 어떤 타입인지(MIME)
 					dataType: "json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열 (생긴게 json이라면) => javascript오브젝트로 변경
 				}).done(function(resp){
-					alert("회원가입이 완료되었습니다.");
-					//console.log(resp);
-					location.href = "/";
+					if(resp.status == 500){
+						alert("존재하는 회원입니다. 로그인 해주십시오.");
+						location.href = "/auth/loginForm";
+					}else{
+						alert("회원가입이 완료되었습니다.");
+						//console.log(resp);
+						location.href = "/";
+					}
 				}).fail(function(error){
 					alert(JSON.stringify(error));
 				});
